3d171090ebddd9145ed2037b6f716dc7728b8788
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-09-24 16:52:50 +0200

    [UPD] used db array to populate dinamically center main section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,18 @@
     <section id="top_main"></section>
 
     <section id="center_main" class="bg-dark">
-        
+        <div class="container">
+            <div class="row">
+                @foreach ($comics_array as $item)
+                    <div class="col-2 d-flex flex-column g-3 py-4">
+                        <div class="resize">
+                            <img src="{{ $item['thumb'] }}" alt="{{ $item['series'] }} cover">
+                        </div>
+                        <p class="text-white pt-3 fs-6">{{ strtoupper($item['series']) }}</p>
+                    </div>
+                @endforeach
+            </div>
+        </div>
 
     </section>
 
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $comics_array = config('db.comics_array');
+    return view('home', compact('comics_array'));
 });
