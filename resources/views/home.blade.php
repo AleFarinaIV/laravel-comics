57c8e117f420283d11ade011e149c2e08bf62915
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-09-24 16:03:12 +0200

    [UPD] main setup, added jumbo bg

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-    
+    <section id="top_main"></section>
+
+    <section id="center_main" class="bg-dark">
+        
+
+    </section>
+
+    <section id="bottom_main"></section>
 @endsection
