002ac361f05bbf470adf04a87b622d533854aefe
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-09-24 15:57:29 +0200

    [UPD] first setup and header creation

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,58 @@
+<header>
+
+    <section id="top_header" class="bg-primary">
+        <div class="container">
+            <div class="row">
+                <div class="offset-8 col-4 d-flex align-items-center text-white py-1">
+                    <span class="fw-semibold">DC POWER<span>&#8480;</span> VISA</span><i class="bi bi-r-circle fs-10"></i>
+                    <select class="btn btn-primary fw-semibold ms-4" name="other_sites" id="other-sites">
+                        <option value="1" selected>ADDITIONAL DC SITES</option>
+                        <option value="">Canada</option>
+                        <option value="3">UK</option>
+                        <option value="4">Australia</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="bottom_header">
+        <div class="container p-3">
+            <div class="row">
+                <div class="col-12 d-flex justify-content-between align-items-center">
+                    <img src="{{Vite::asset('../resources/img/dc-logo.png')}}" alt="dc-logo">
+    
+                    <ul class="list-unstyled d-flex align-items-center fw-semibold">
+    
+                        <li>CHARACTERS</li>
+                        <li>COMICS</li>
+                        <li>MOVIES</li>
+                        <li>TV</li>
+                        <li>GAMES</li>
+                        <li>COLLECTIBLES</li>
+                        <li>VIDEOS</li>
+                        <li>FANS</li>
+                        <li>NEWS</li>
+                        <li><div class="dropdown p-0 m-0">
+                            <button class="btn btn-white dropdown-toggle fw-semibold" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                              SHOP
+                            </button>
+                            <ul class="dropdown-menu">
+                              <li><a class="dropdown-item" href="#">Action</a></li>
+                              <li><a class="dropdown-item" href="#">Another action</a></li>
+                              <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                          </div>
+                        </li>
+                    </ul>
+
+                    <div class=" mb-3 border-bottom border-primary border-2">
+                        <input type="text" class="text-dark fw-semibold border-0 text-end" placeholder="Search" aria-label="Username" aria-describedby="basic-addon1">
+                        <span class="fw-bold" id="basic-addon1"><i class="bi bi-search"></i></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</header>
